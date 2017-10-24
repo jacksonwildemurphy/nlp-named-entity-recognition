03da16c26dd5ebb6fd9d4d3743efcd1017224f41
Jackson Murphy <jackson@Jacksons-MacBook-Air.local>
2017-10-24 16:41:13 -0600

    writing to readable file is working

--- ner.py
+++ ner.py
@@ -1,6 +1,6 @@
 # Written by Jackson Murphy.
 
-#import Set as set
+import re
 import sys
 
 # Returns a set of all the locations provided in the locations file
@@ -63,9 +63,9 @@ def _add_word(sentence, i, features, feature_ids, current_id):
         current_id[0] += 1
 
 # Adds the words before and after sentence[i][2] to the feature id dictionary,
-# as long as WORDCONTEXT was provided in the command line arguments
+# as long as WORDCON was provided in the command line arguments
 def _add_word_context(sentence, i, features, feature_ids, current_id):
-    if "WORDCONTEXT" not in features:
+    if "WORDCON" not in features:
         return
 
     add_prev_word = True
@@ -99,9 +99,9 @@ def _add_pos(sentence, i, features, feature_ids, current_id):
         current_id[0] += 1
 
 # Adds the pos tags before and after sentence[i][1] to the feature id dictionary,
-# as long as POSCONTEXT was provided in the command line arguments
+# as long as POSCON was provided in the command line arguments
 def _add_pos_context(sentence, i, features, feature_ids, current_id):
-    if "POSCONTEXT" not in features:
+    if "POSCON" not in features:
         return
 
     add_prev_pos = True
@@ -114,13 +114,13 @@ def _add_pos_context(sentence, i, features, feature_ids, current_id):
         add_next_pos = False
 
     if add_prev_pos:
-        prev_pos = sentence[i-1][2]
+        prev_pos = sentence[i-1][1]
         if ("prev-pos-" + prev_pos) not in feature_ids:
             feature_ids["prev-pos-" + prev_pos] = current_id[0]
             current_id[0] += 1
 
     if add_next_pos:
-        next_pos = sentence[i+1][2]
+        next_pos = sentence[i+1][1]
         if ("next-pos-" + next_pos) not in feature_ids:
             feature_ids["next-pos-" + next_pos] = current_id[0]
             current_id[0] += 1
@@ -150,7 +150,7 @@ def _add_location(features, locations, feature_ids, current_id):
 def _add_pseudos(feature_ids, current_id):
     feature_ids["word-UNK"] = current_id[0]; current_id[0] += 1
 
-    if "WORDCONTEXT" in features:
+    if "WORDCON" in features:
         feature_ids["prev-word-UNK"] = current_id[0]; current_id[0] += 1
         feature_ids["next-word-UNK"] = current_id[0]; current_id[0] += 1
         feature_ids["prev-word-PHI"] = current_id[0]; current_id[0] += 1
@@ -159,12 +159,124 @@ def _add_pseudos(feature_ids, current_id):
     if "POS" in features:
         feature_ids["pos-UNKPOS"] = current_id[0]; current_id[0] += 1
 
-    if "POSCONTEXT" in features:
+    if "POSCON" in features:
         feature_ids["prev-pos-UNKPOS"] = current_id[0]; current_id[0] += 1
         feature_ids["next-pos-UNKPOS"] = current_id[0]; current_id[0] += 1
         feature_ids["prev-pos-PHIPOS"] = current_id[0]; current_id[0] += 1
         feature_ids["next-pos-OMEGAPOS"] = current_id[0]; current_id[0] += 1
 
+def _generate_files_from_training_set(training_file_str, feature_ids, locations, features):
+    # The two output files we are creating and will write to
+    readable_file = open(training_file_str + ".readable", "w+")
+    vector_file = open(training_file_str + ".vector", "w+")
+
+    training_file = open(training_file_str)
+    sentence = [] # initialize the first sentence
+    line = training_file.readline()
+
+    while line:
+        # Build up a sentence like so: [[B-LOC,NNP,Israel], [O,NN,television], ...]
+        if line.strip():
+            sentence.append(line.split())
+        else:
+            if len(sentence) != 0: # Nec. bc there can be consecutive blank lines
+                _write_sentence_to_readable(sentence, features, locations, readable_file)
+                #_write_sentence_to_vector(sentence, features, locations, feature_ids, vector_file)
+                sentence.clear() # empty the list to accommodate next sentence
+        line = training_file.readline()
+
+    training_file.close()
+    readable_file.close()
+    vector_file.close()
+
+
+
+def _write_sentence_to_readable(sentence, features, locations, readable_file):
+    # Write info to readable file for each word in the sentence
+    for i in range(len(sentence)):
+        readable_file.write("WORD: " +  sentence[i][2] +  "\n")
+        readable_file.write("WORDCON: " +  _get_word_context(sentence,i, features, "train") + "\n")
+        readable_file.write("POS: " + _get_pos(sentence, i, features, "train") + "\n")
+        readable_file.write("POSCON: " + _get_pos_context(sentence, i, features, "train") + "\n")
+        readable_file.write("ABBR: " + _get_abbreviation(sentence, i, features, "train") + "\n")
+        readable_file.write("CAP: " + _get_capitalization(sentence, i, features, "train") + "\n")
+        readable_file.write("LOCATION: " + _get_location(sentence, i, features, locations, "train") + "\n")
+        readable_file.write("\r\n")
+
+def _get_word_context(sentence, i, features, set_type):
+    if "WORDCON" not in features:
+        return "n/a"
+
+    prev_word, next_word = "", ""
+    got_prev_word, got_next_word = False, False
+
+    if i == 0: # First word in the sentence
+        prev_word = "PHI"
+        got_prev_word = True
+    elif i == (len(sentence)-1): # Last word in the sentence
+        next_word = "OMEGA"
+        got_next_word = True
+    if not got_prev_word:
+        prev_word = sentence[i-1][2]
+    if not got_next_word:
+        next_word = sentence[i+1][2]
+
+    return prev_word + " " + next_word
+
+def _get_pos(sentence, i, features, set_type):
+    if "POS" not in features:
+        return "n/a"
+    else:
+        return sentence[i][1]
+
+def _get_pos_context(sentence, i, features, set_type):
+    if "POSCON" not in features:
+        return "n/a"
+    prev_pos, next_pos = "", ""
+    got_prev_pos, got_next_pos = False, False
+
+    if i == 0: # First pos in the sentence
+        prev_pos = "PHIPOS"
+        got_prev_pos = True
+    elif i == (len(sentence)-1): # Last pos in the sentence
+        next_pos = "OMEGAPOS"
+        got_next_pos = True
+    if not got_prev_pos:
+        prev_pos = sentence[i-1][1]
+    if not got_next_pos:
+        next_pos = sentence[i+1][1]
+
+    return prev_pos + " " + next_pos
+
+def _get_abbreviation(sentence, i, features, set_type):
+    if "ABBR" not in features:
+        return "n/a"
+    word = sentence[i][2]
+    # Abbreviations end with a period and consists entirely of alphabetic
+    # characters and 1 or more periods, and are less than 5 characters
+    pattern = re.compile("^(.*[a-zA-Z]+.*)*)$")
+    if pattern.match(word) and len(word) < 5:
+        return "yes"
+    else:
+        return "no"
+
+def _get_capitalization(sentence, i, features, set_type):
+    if "CAP" not in features:
+        return "n/a"
+    word = sentence[i][2]
+    if word.istitle() or word.isupper():
+        return "yes"
+    else:
+        return "no"
+
+def _get_location(sentence, i, features, locations, set_type):
+    if "LOCATION" not in features:
+        return "n/a"
+    word = sentence[i][2]
+    if word in locations:
+        return "yes"
+    else:
+        return "no"
 
 ##### START OF PROGRAM #####
 
@@ -177,29 +289,7 @@ locations = _get_locations(sys.argv[3])
 features = _get_features(sys.argv)
 feature_ids = _create_feature_ids(sys.argv[1], features, locations)
 
-for item in sorted(feature_ids.items(), key=lambda x: x[1], reverse=True):
-    print(item)
+_generate_files_from_training_set(sys.argv[1], feature_ids, locations, features)
 
-#
-# training_file = open("practice.txt")
-# processed_sentence = False
-# sentence = []
-# line = training_file.readline()
-#
-# while not line.strip()
-#     line = training_file.readline()
-#
-# while line:
-#     processed_sentence = False
-#     while line.strip():
-#         print("In the loop")
-#         sentence.append(line.split())
-#         line = training_file.readline()
-#         processed_sentence = True
-#
-#         if not processed_sentence:
-#             line = training_file.readline()
-#
-#
-#     print("End of sentence:", sentence)
-#     sentence = []
+# for item in sorted(feature_ids.items(), key=lambda x: x[1], reverse=True):
+#     print(item)
